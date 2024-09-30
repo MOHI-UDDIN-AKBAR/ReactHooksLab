f214cea578912ead8be3132a52102c0ae67dd635
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2024-09-30 19:36:10 +0200

    Export React components

---
+++ src/custom-hooks/index.tsx
@@ -0,0 +1,35 @@
+import ToggleComponent from './components/ToggleComponent';
+import TimeoutComponent from './components/TimeoutComponent';
+import DebounceComponent from './components/DebounceComponent';
+import UpdateEffectComponent from './components/UpdateEffectComponent';
+import ArrayComponent from './components/ArrayComponent';
+import PreviousComponent from './components/PreviousComponent';
+import StateHistoryComponent from './components/StateHistoryComponent';
+import StorageComponent from './components/StorageComponent';
+import AsyncComponent from './components/AsyncComponent';
+import FetchComponent from './components/FetchComponent';
+import DeepCompareEffectComponent from './components/DeepCompareEffectComponent';
+import EventListenerComponent from './components/EventListenerComponent';
+import OnScreenComponent from './components/OnScreenComponent';
+import WindowSizeComponent from './components/WindowSizeComponent';
+import MediaQueryComponent from './components/MediaQueryComponent';
+import GeolocationComponent from './components/GeolocationComponent';
+
+export {
+  ToggleComponent,
+  TimeoutComponent,
+  DebounceComponent,
+  UpdateEffectComponent,
+  ArrayComponent,
+  PreviousComponent,
+  StateHistoryComponent,
+  StorageComponent,
+  AsyncComponent,
+  FetchComponent,
+  DeepCompareEffectComponent,
+  EventListenerComponent,
+  OnScreenComponent,
+  WindowSizeComponent,
+  MediaQueryComponent,
+  GeolocationComponent,
+};
