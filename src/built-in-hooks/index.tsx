65bce087d4f928198aa2515323f926c30ccb4212
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2024-09-30 19:36:54 +0200

    Export React components

--- src/built-in-hooks/index.tsx
+++ src/built-in-hooks/index.tsx
@@ -1,3 +1,39 @@
-import UseStatePractice from "./components/UseStatePractice";
-import UseEffectPractice from "./components/UseEffectPractice";
-export { UseStatePractice, UseEffectPractice };
+import UseStatePractice from './components/UseStatePractice';
+import UseEffectPractice from './components/UseEffectPractice';
+import UseLayoutEffectPractice from './components/UseLayoutEffectPractice';
+import UseRefPractice from './components/UseRefPractice';
+import UseReducerPractice from './components/UseReducerPractice';
+import UseCallbackPractice from './components/UseCallbackPractice';
+import UseMemoPractice from './components/UseMemoPractice';
+import UseContextPractice from './components/UseContextPractice';
+import NormalComponent from './components/NormalComponent';
+import UseTransitionPractice from './components/UseTransitionPractice';
+import UseDeferredValuePractice from './components/UseDeferredValuePractice';
+import UseImperativeHandlePractice from './components/UseImperativeHandlePractice';
+import UseDebugValuePractice from './components/UseDebugValuePractice';
+import JustPractice from './components/JustPractice';
+import JustPracticeTwo from './components/JustPracticeTwo';
+import JustPracticeThree from './components/JustPracticeThree';
+import JustPracticeFour from './components/JustPracticeFour';
+import NormalComponentTwo from './components/NormalComponentTwo';
+
+export {
+  UseStatePractice,
+  UseEffectPractice,
+  UseLayoutEffectPractice,
+  UseRefPractice,
+  UseReducerPractice,
+  UseCallbackPractice,
+  UseMemoPractice,
+  UseContextPractice,
+  NormalComponent,
+  UseTransitionPractice,
+  UseDeferredValuePractice,
+  UseImperativeHandlePractice,
+  UseDebugValuePractice,
+  JustPractice,
+  JustPracticeTwo,
+  JustPracticeThree,
+  JustPracticeFour,
+  NormalComponentTwo,
+};
